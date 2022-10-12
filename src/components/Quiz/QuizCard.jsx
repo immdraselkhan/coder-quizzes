4f730b9109480882ed22b68c5763ab051158def1
Md Rasel Khan <immdraselkhan@gmail.com>
2022-10-12 17:44:04 +0600

    quiz final

--- src/components/Quiz/QuizCard.jsx
+++ src/components/Quiz/QuizCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid';
 import QuizOptions from './QuizOptions';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 
 const QuizCard = ({everyQuestion, index, handleResult}) => {
   const {question, options, correctAnswer} = everyQuestion;
@@ -12,11 +12,22 @@ const QuizCard = ({everyQuestion, index, handleResult}) => {
     setDisable('disabled');
   };
 
+  const quickResult = answer => {
+    toast((t) => (
+      <span className="font-medium flex items-center gap-5">
+        {answer}
+        <button className="text-white px-2 py-1 bg-[#5f49eb] hover:bg-[#3a28a7] rounded" onClick={() => toast.dismiss(t.id)}>Dismiss</button>
+      </span>
+    ));
+  };
+
   return (
     <div className="bg-[#5f49eb] p-10 space-y-5 rounded-lg">
       <div className="flex justify-between gap-5 pb-5 text-white">
         <p className="text-lg font-medium leading-8">Quiz {index+1}: {`${question.replace(/[<p></p>]/g, '')}`}</p>
-        <EyeIcon className="h-10 w-10 cursor-pointer"/>
+        <div className="h-10 w-10">
+          <EyeIcon onClick={() => quickResult(correctAnswer)} className="h-10 w-10 cursor-pointer"/>
+        </div>
       </div>
       <div className={`space-y-5 ${disable ? disable : ''}`}>
         { options.map(option => <QuizOptions key={options.indexOf(option)} option={option} correctAnswer={correctAnswer} disableHandler={disableHandler} handleResult={handleResult} />) }
